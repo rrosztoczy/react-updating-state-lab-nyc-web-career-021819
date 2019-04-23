ee66f4af1673b2ad34ea4e2098ee3a500cb062f5
rrosztoczy <rrosztoczy@gmail.com>
2019-04-23 17:12:48 -0400

    done

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
-// Code DigitalClicker Component Here
+// // Code DigitalClicker Component Here
+// In the components/DigitalClicker.js file, create a DigitalClicker React component.
+
+// This component has an initial state property called timesClicked, which is initially defined as 0.
+
+// The component renders out a button with a label that shows the timesClicked value. 
+// This means that, at the start, your button should just say 0.
+
+// Whenever the button is clicked, update the state by incrementing the timesClicked by 1.
+
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+
+  state = {
+    timesClicked: 0
+  }
+
+  incrementClick = () => {
+    // const newState = prevState + 1
+    this.setState( prevState => ({timesClicked: prevState.timesClicked + 1}), prevState => (console.log("prev state:", prevState, "newState:", this.state )))
+  }
+
+  render () {
+    return <button onClick={this.incrementClick}>{this.state.timesClicked}</button>
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,78 @@
-// Code YouTubeDebugger Component Here
+// // Code YouTubeDebugger Component Here
+
+// In the components/YouTubeDebugger.js file, create a YouTubeDebugger React component.
+
+// This component has several state properties. The initial state shape looks like this:
+
+// {
+//   errors: [],
+//   user: null,
+//   settings: {
+//     bitrate: 8,
+//     video: {
+//       resolution: '1080p'
+//     }
+//   }
+// }
+// Create a button with the class 'bitrate'. Clicking this button changes 
+// the settings.bitrate state property to 12.
+
+// Create a button with the class 'resolution'. Clicking this button changes the 
+// settings.video.resolution state property to '720p'.
+
+import React, { Component } from 'react';
+
+
+
+
+export default class YouTubeDebugger extends Component {
+
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: '1080p'
+        }
+    }
+  }
+
+  // this.setState({
+  //   addressInfo: {
+  //     ...this.state.addressInfo,
+  //     city: 'New York City'
+  //   }
+  // });
+
+  setBitrate = () => {
+   this.setState({
+     settings: {
+       ...this.state.settings,
+       bitrate: 12
+     }
+   }) 
+  }
+
+  setResolution = () => {
+    this.setState({
+        settings: {
+          ...this.state.settings,
+          video: {
+            ...this.state.settings.video,
+            resolution: '720p'
+          }
+        }
+    }) 
+   }
+
+  render() {
+
+    return (
+    <div>
+      <button className ="bitrate" onClick={this.setBitrate}></button>
+      <button className ="resolution" onClick={this.setResolution}></button>
+    </div>
+    )
+  }
+}
